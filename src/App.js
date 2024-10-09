94e3bb7071425cbd320b1c262d20471844766fb4
Marwa Refaieh <marwarefaieh@gmail.com>
2024-10-09 16:09:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  // State 1
+  // useEffect(()=>{
+  //   console.log("useEffect");
+  // },)
+
+  // State 2
+  // useEffect(()=>{
+  //   console.log("useEffect");
+  // },[])
+
+  // State 3
+  // useEffect(()=>{
+  //   console.log("useEffect");
+  // },[counter1])
+
+  // State 4
+  useEffect(() => {
+    console.log("useEffect");
+  }, [counter1, counter2]);
+
+  // Timer
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const time = setInterval(() => {
+      setTimer(timer + 1);
+    }, 1000);
+    return () => {
+      clearInterval(time);
+    };
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{counter1}</h2>
+      <button
+        onClick={() => {
+          setCounter1(counter1 + 1);
+        }}
+      >
+        +
+      </button>
+
+      <br />
+
+      <h2>{counter2}</h2>
+      <button
+        onClick={() => {
+          setCounter2(counter2 + 1);
+        }}
+      >
+        +
+      </button>
+
+      <hr />
+
+      <h2>Timer</h2>
+      <p>{timer} sec</p>
+      <p> {Math.floor(timer / 60)} min</p>
+      <p> {Math.floor(timer / 3600)} houre</p>
     </div>
   );
 }
